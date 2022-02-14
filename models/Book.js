22c524a2dd6bf7e829cd0859aee55109e5950844
Jonathan Delgado <nathandelgado.dv@gmail.com>
2022-02-14 15:42:13 -0500

    Create Book model

---
+++ models/Book.js
@@ -0,0 +1,47 @@
+const {
+    Schema,
+    model,
+} = require('mongoose');
+
+const BookSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    author: {
+        type: String,
+        requiered: true
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Categorie',
+        requiered: true
+    },
+    amount: {
+        type: Number,
+        default: 1
+    },
+    borrowedToUser: {
+        type: Array,
+        default: []
+    },
+    status: {
+        type: Boolean,
+        default: true
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    },
+    updatedAt: {
+        type: Date
+    }
+});
+
+BookSchema.methods.toJSON = function() {
+    const { __v, updatedAt, createdAt, ...book } = this.toObject();
+    return book;
+};
+
+module.exports = model('Book', BookSchema);
